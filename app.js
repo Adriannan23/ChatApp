a1d4444581e40ee1ce0c919973f0749905b52c64
Adrianna Nowak <adriannanowakk@gmail.com>
2023-09-20 17:34:29 +0200

    add addMessage function and off autocomplete

--- app.js
+++ app.js
@@ -14,23 +14,45 @@ const messageContentInput = document.getElementById('message-content');
 // global variable
 let userName;
 
-
 const login = e => {
   e.preventDefault();
-
   if (userNameInput.value === '') {
-    console.log('empty');
+    console.log('empty value in the login input');
   }
   else {
     userName = userNameInput.value;
-    console.log(userName);
     loginForm.classList.remove('show');
-    addMessageForm.classList.add('show');
-    console.log('usuwam login, dodaje messageform');
-  }
-
+    messagesSection.classList.add('show');
+  };
 };
 
+function addMessage(author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if (author === userName) message.classList.add('message--self');
+  message.innerHTML = `
+    <h3 class="message__author">${userName === author ? 'You' : author}</h3>
+    <div class="message__content">
+      ${content}
+    </div>
+  `;
+  messagesList.appendChild(message);
+}
+
+const sendMessage = e => {
+  e.preventDefault();
+
+  if (userNameInput.value === '') {
+    console.log('empty value in the message input');
+  }
+  else {
+    addMessage(userName, messageContentInput.value);
+    messageContentInput.value = '';
+  };
+}
+
 loginForm.addEventListener('submit', login);
+addMessageForm.addEventListener('submit', sendMessage)
 
 
