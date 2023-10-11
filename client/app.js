845744a75f36deee3112cb20aab6635b5c99ffb1
Adrianna Nowak <adriannanowakk@gmail.com>
2023-10-11 13:45:42 +0200

    x

--- client/app.js
+++ client/app.js
@@ -11,6 +11,10 @@ const userNameInput = document.getElementById('username');
 
 const messageContentInput = document.getElementById('message-content');
 
+const socket = io();
+
+socket.on('message', addMessage);
+socket.emit('message', { author: 'John Doe', content: 'Lorem Ipsum' });
 // global variable
 let userName;
 
@@ -40,19 +44,21 @@ function addMessage(author, content) {
   messagesList.appendChild(message);
 }
 
-const sendMessage = e => {
+function sendMessage(e) {
   e.preventDefault();
 
-  if (userNameInput.value === '') {
-    alert('empty value in the message input');
+  let messageContent = messageContentInput.value;
+
+  if (!messageContent.length) {
+    alert('You have to type something!');
   }
   else {
-    addMessage(userName, messageContentInput.value);
+    addMessage(userName, messageContent);
+    socket.emit('message', { author: userName, content: messageContent })
     messageContentInput.value = '';
-  };
+  }
+
 }
 
 loginForm.addEventListener('submit', login);
 addMessageForm.addEventListener('submit', sendMessage)
-
-
